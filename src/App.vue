bc2e75fde92a7934d9dd7191c28784e26c0f1f89
小样儿邓 <1219575252@qq.com>
2021-10-19 19:24:55 +0800

    feat:调整目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>denglanjuan</h1>
+    <!-- 路由入口 -->
+    <router-view />
   </div>
 </template>
 
